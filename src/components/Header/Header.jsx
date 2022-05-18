092570d0715ca8dfd406443d3440ab74491ff539
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-18 08:50:15 +0430

    responsive for all device

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,16 @@ import photoMe_min from '../../asset/image/photoMe_min.png';
 
 const Header = () => {
 	return (
-		<header className='w-full px-4 pt-3'>
-			<section className='flex items-center justify-between'>
-				<p className='text-lg font-bold'>Todo App</p>
+		<header className='flex w-full flex-col items-center justify-center px-4 pt-3 sm:px-5 sm:py-3'>
+			<section className='flex w-full items-center justify-between '>
+				<p className='text-lg font-bold sm:text-xl'>Todo App</p>
 
-				<div className='h-8 w-8 rounded-full border-2'>
+				<div className='h-8 w-8 rounded-full border-2 sm:h-10 sm:w-10'>
 					<img src={photoMe_min} className='h-full w-full' alt='mh pourhasani photo' />
 				</div>
 			</section>
 
-			<SearchAndFilter />
+			<SearchAndFilter className='w-full' />
 		</header>
 	);
 };
