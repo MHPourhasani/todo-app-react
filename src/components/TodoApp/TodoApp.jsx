31771008ef1ad7473b62d34b5bb76ea59959fa4e
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-18 06:32:14 +0430

    add TodoList comp in TodoApp

--- src/components/TodoApp/TodoApp.jsx
+++ src/components/TodoApp/TodoApp.jsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 import AddTodoModal from '../AddTodo/AddTodoModal';
 import Header from '../Header/Header';
+import TodoList from '../TodoList/TodoList';
 
 const TodoApp = () => {
 	const [todos, setTodos] = useState([]);
 
 	const addTodoHandler = (input) => {
-        const newTodo = {
-            id: Math.floor(Math.random() * 1000),
+		const newTodo = {
+			id: Math.floor(Math.random() * 1000),
 			title: input,
 			isCompleted: false,
 		};
-		setTodos(newTodo);
+		setTodos([...todos, newTodo]);
 	};
-    console.log(todos);
+	console.log(todos);
 
 	return (
 		<>
 			<Header />
 			<AddTodoModal addTodoHandler={addTodoHandler} />
+			<TodoList todos={todos} />
 		</>
 	);
 };
