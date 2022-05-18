eb56cc059c42b54f069ee96332902f80b1286599
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-18 08:49:53 +0430

    responsive for all device

--- src/components/Header/SearchAndFilter.jsx
+++ src/components/Header/SearchAndFilter.jsx
@@ -2,16 +2,16 @@ import { BiSearch } from 'react-icons/bi';
 
 const SearchAndFilter = () => {
 	return (
-		<div className='flex w-11/12 items-center justify-between pt-2'>
+		<div className='flex w-full items-center justify-between mx-4 mt-3 md:w-8/12 '>
 			<input
 				type='text'
 				placeholder='Search your Todo ...'
-				className='mr-2 h-8 w-11/12 rounded-md border-2 border-violet-600 pl-2 text-sm shadow-sm outline-none focus:bg-violet-100'
+				className='mr-2 h-8 w-11/12 rounded-md border-2 border-violet-600 pl-2 text-sm shadow-sm outline-none focus:bg-violet-100 sm:h-10'
 			/>
 
 			<button
 				type='submit'
-				className='flex h-8 w-8 items-center justify-center rounded-md bg-violet-300 shadow-sm'>
+				className='flex h-8 w-8 items-center justify-center rounded-md bg-violet-300 shadow-sm sm:h-10 sm:w-10'>
 				<BiSearch />
 			</button>
 		</div>
