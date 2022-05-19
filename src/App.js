c90fdd560140d0606822664e7a24399d0431d14f
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-19 05:29:28 +0430

    import react toastify component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import TodoApp from './components/TodoApp/TodoApp';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
 	return (
 		<div className=''>
+			<ToastContainer />
 			<TodoApp />
 		</div>
 	);
