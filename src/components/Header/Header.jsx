660db3f4614db9b462a23e266b3dfb8a0dbb4ec8
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-19 21:18:38 +0430

    change my photo in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,20 @@
 import SearchAndFilter from './SearchAndFilter';
-import photoMe_min from '../../asset/image/photoMe_min.png';
+import myPhotoComp from '../../asset/image/myPhotoComp.jpg';
 
 const Header = () => {
 	return (
 		<header className='flex w-full flex-col items-center justify-center px-4 pt-3 sm:px-5 sm:py-3'>
-			<section className='flex w-full items-center justify-between '>
+			<section className='flex w-full items-center justify-between lg:my-5 lg:w-1/2'>
 				<p className='text-lg font-bold sm:text-xl'>Todo App</p>
 
-				<div className='h-8 w-8 rounded-full border-2 sm:h-10 sm:w-10'>
-					<img src={photoMe_min} className='h-full w-full' alt='mh pourhasani photo' />
-				</div>
+				<a href='https://github.com/MHPourhasani' target='_blank' rel='noreferrer'>
+					<img
+						src={myPhotoComp}
+						aria-hidden
+						className='h-8 w-8 cursor-pointer rounded-full'
+						alt='mh pourhasani photo'
+					/>
+				</a>
 			</section>
 
 			<SearchAndFilter className='w-full' />
