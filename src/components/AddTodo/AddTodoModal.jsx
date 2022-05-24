c3c5d6802c596ad096e568e5860e75f22dbc0b43
Mohammad Hasan Pouhasani <mr.se.mhp@gmail.com>
2022-05-24 21:03:54 +0430

    add toast from react toastify for add todo

--- src/components/AddTodo/AddTodoModal.jsx
+++ src/components/AddTodo/AddTodoModal.jsx
@@ -1,16 +1,20 @@
 import { useEffect, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 
 function AddTodoModal({ addTodoHandler }) {
 	const [showModal, setShowModal] = useState(false);
 	const [inputValue, setInputValue] = useState('');
 
 	const inputRef = useRef(null);
+	const formRef = useRef(null);
 
-	useEffect(() => {
-		// inputRef.current.focus();
-	}, []);
+	// useEffect(() => {
+	// 	// inputRef.current.focus();
+	// }, []);
 
-	useEffect(() => {}, [showModal]);
+	// useEffect(() => {
+		
+	// }, [showModal]);
 
 	const changeHandler = (e) => {
 		setInputValue(e.target.value);
@@ -20,10 +24,20 @@ function AddTodoModal({ addTodoHandler }) {
 		e.preventDefault();
 		if (inputValue) {
 			addTodoHandler(inputValue);
-			setInputValue('');
+
+			toast.success('added your todo!', {
+				position: "top-right",
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: true,
+				draggable: true,
+				progress: undefined,
+				});
 		} else {
 			<p className='text-red-600'>Invalid Value ...</p>;
 		}
+		setInputValue('');
 	};
 
 	return (
@@ -37,9 +51,10 @@ function AddTodoModal({ addTodoHandler }) {
 
 			{showModal ? (
 				<form
+				ref={formRef}
 					onSubmit={submitHandler}
 					className='flex w-11/12 flex-col items-start justify-center'>
-					<div className='fixed inset-0 z-50 mx-auto flex w-full items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none sm:w-8/12'>
+					<div className='fixed inset-0 z-50 mx-auto flex w-full items-center justify-center overflow-y-auto overflow-x-hidden outline-none sm:w-8/12 lg:w-5/12'>
 						<div className='relative my-6 mx-auto w-11/12 max-w-3xl'>
 							<div className='relative flex w-full flex-col justify-center rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none'>
 								<h3 className='px-5 pt-5 text-xl font-bold'>Add Todo</h3>
@@ -51,22 +66,22 @@ function AddTodoModal({ addTodoHandler }) {
 										ref={inputRef}
 										onChange={changeHandler}
 										placeholder='Todo Title ...'
-										className='my-1 w-full rounded-md border-2 border-gray-400 py-1 pl-2 text-sm leading-relaxed text-slate-500 outline-none focus:border-violet-500 sm:py-2'
+										className='my-1 w-full rounded-md border-2 border-gray-400 py-1 pl-2 text-sm leading-relaxed text-black outline-none focus:border-violet-500 sm:py-2'
 									/>
 								</div>
 
 								<section className='flex items-center justify-end rounded-b p-6'>
 									<button
-										className='background-transparent mr-1 mb-1 px-6 py-2 text-xs font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none sm:py-3 sm:text-xs'
+										className='background-transparent mr-1 mb-1 rounded-md px-6 py-2 text-xs font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear hover:bg-red-200 focus:outline-none sm:py-3 sm:text-xs'
 										type='button'
 										onClick={() => setShowModal(false)}>
 										Close
 									</button>
 
 									<button
-										className='mr-1 mb-1 rounded bg-violet-500 px-3 py-2 text-xs font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-violet-600 sm:px-4 sm:py-3 sm:text-xs'
+										className='mr-1 mb-1 rounded bg-violet-500 px-3 py-2 text-xs font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:bg-violet-600 focus:outline-none active:bg-violet-600 sm:px-4 sm:py-3 sm:text-xs'
 										type='submit'
-										onClick={() => setShowModal(false)}
+										// onClick={() => setShowModal(false)}
 									>
 										Add Todo
 									</button>
